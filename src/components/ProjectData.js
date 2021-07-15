799f99471b5b91e53dfe7e4c1bcfbdf09cc19396
Cavin Gayle <cavingayle@gmail.com>
2021-07-15 16:43:25 -0400

    Added Vintage Shop Project

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,35 +1,50 @@
 export const projectData = [
-    {
-    projectTitle:'Youtube Yelp',
-    projectDescription:'Led team of developers to create a social media platform that allows users to review youtube channels',
-    projectTech: ['React', 'Node', 'Express', 'Redux', 'S3','PostgreSQL'],
-    githubLink:'https://github.com/cavingayle/youtubeyelp',
-    liveLink:'http://youtube-yelp.com/#/'
-  },
-    {
-    projectTitle:'Healthify',
-    projectDescription:'A subscription based healthy snack and food ecommerce website ',
-    projectTech: ['React', 'Node','PostgreSQL', 'Express', 'Twilio', 'Stripe', 'Redux' ],
-    githubLink:'https://github.com/cavingayle/dev-personal-project/',
-    liveLink:'http://142.93.253.118:4003/#/'
-
+  {
+    projectTitle: 'Youtube Yelp',
+    projectDescription:
+      'Led team of developers to create a social media platform that allows users to review youtube channels',
+    projectTech: ['React', 'Node', 'Express', 'Redux', 'S3', 'PostgreSQL'],
+    githubLink: 'https://github.com/cavingayle/youtubeyelp',
+    liveLink: 'http://youtube-yelp.com/#/',
   },
   {
-    projectTitle:'Lang Fit',
-    projectDescription:'Designed and developed a very clean and simple mock fitness website',
-    projectTech: ['React', 'Styled Components', 'React Icons', 'React Router', ],
-    githubLink:'https://github.com/cavingayle/Fitness-Website',
-    liveLink:'https://langfitness.netlify.app/'
-
+    projectTitle: 'Healthify',
+    projectDescription:
+      'A subscription based healthy snack and food ecommerce website ',
+    projectTech: [
+      'React',
+      'Node',
+      'PostgreSQL',
+      'Express',
+      'Twilio',
+      'Stripe',
+      'Redux',
+    ],
+    githubLink: 'https://github.com/cavingayle/dev-personal-project/',
+    liveLink: '',
   },
-    {
-    projectTitle:'Component Library',
-    projectDescription:'Constructed reusable components utilizing react best practices in order build a component library',
-    projectTech: ['React', 'Styled Components', 'Storybook', 'Atomic Design', ],
-    githubLink:'https://github.com/cavingayle/Storybook-StyledComponents',
-    liveLink:'https://5fd9716c9a2cde16086753e3--buttons-storybook.netlify.app/?path=/story/main-button--template'
-
+  {
+    projectTitle: 'Vintage Shop',
+    projectDescription: 'A minimal designed ecommerce styled website',
+    projectTech: ['HTML', 'CSS', 'Responsive Design'],
+    githubLink: 'https://github.com/cavingayle/Vintage-Shop',
+    liveLink: 'https://vintageshop.netlify.app/',
   },
-   
-  
-  ]
+  {
+    projectTitle: 'Lang Fit',
+    projectDescription:
+      'Designed and developed a very clean and simple mock fitness website',
+    projectTech: ['React', 'Styled Components', 'React Icons', 'React Router'],
+    githubLink: 'https://github.com/cavingayle/Fitness-Website',
+    liveLink: 'https://langfitness.netlify.app/',
+  },
+  {
+    projectTitle: 'Component Library',
+    projectDescription:
+      'Constructed reusable components utilizing react best practices in order build a component library',
+    projectTech: ['React', 'Styled Components', 'Storybook', 'Atomic Design'],
+    githubLink: 'https://github.com/cavingayle/Storybook-StyledComponents',
+    liveLink:
+      'https://5fd9716c9a2cde16086753e3--buttons-storybook.netlify.app/?path=/story/main-button--template',
+  },
+]
